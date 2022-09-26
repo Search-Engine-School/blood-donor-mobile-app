d768dd78d375d242d8adfeb7282cecf7eef424e9
kamrulhaider <kamrulhaider.ssb@gmail.com>
2022-09-26 10:43:12 +0600

    Search Blood Page

---
+++ component/SearchBlood/SearchBlood.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function SearchBlood() {
+  return (
+    <View>
+      <Text>SearchBlood</Text>
+    </View>
+  );
+}
